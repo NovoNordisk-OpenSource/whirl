cf4993917d3bf736bf4a3ee34fb0b4e439f08fb3
Lovemore <LVGK@novonordisk.com>
2024-03-08 13:50:37 +0000

    feat: remove retrieve_fpath to a separate file.

--- R/log.R
+++ R/log.R
@@ -52,31 +52,6 @@ log_example <- function(doc) {
   system.file("examples", doc, package = "whirl")
 }
 
-#' Retrieve path of file from folder/subfolders
-#'
-#' @param in_file - file to search path for
-#'
-#' @return A path to a file
-#' @export
-#'
-#' @examples
-#' retrieve_fpath("prg1.R")
-retrieve_fpath <- function(in_file) {
-  tryCatch(
-    expr = {
-      list.files(
-        pattern = in_file,
-        recursive = TRUE,
-        full.names = TRUE,
-        include.dirs = TRUE
-      )[[1]] |> normalizePath(winslash = "/")
-    },
-    error = function(e){
-      stop(paste("File does not seem to exist: ", in_file))
-    }
-  )
-}
-
 #' `quarto::quarto_render()`, but output file is moved to `output_dir`
 #'
 #' The default `quarto::quarto_render()` function can only render outputs
