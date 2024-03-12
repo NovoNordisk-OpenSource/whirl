8234e08ed8bcb401f2acc49667b0413e64c62240
LVGK (Lovemore Gakava) <lvgk@novonordisk.com>
2024-03-12 15:38:32 +0100

    Feat: Add strace functionality to get information about files.

--- inst/examples/run_scripts_examples.R
+++ inst/examples/run_scripts_examples.R
@@ -4,11 +4,21 @@
 
 run_script(
   script = retrieve_fpath("prgQmd.qmd"),
-  strace = FALSE,
+  strace = TRUE,
   renv = FALSE,
-  cleanup = TRUE,
+  cleanup = FALSE,
   output_dir = "./inst/output"
 )
+
+run_script(
+  script = "/scer/homedirs/lvgk/training/nn1234/nn1234-0002/current/stats/program/statprog/box_plot.R",
+  strace = TRUE,
+  renv = FALSE,
+  cleanup = FALSE,
+  output_dir = "./inst/output"
+)
+
+
 
 run_script(
   retrieve_fpath("prg1.R"),
@@ -25,3 +35,13 @@ run_script(
   cleanup = TRUE,
   output_dir = "./inst/output"
 )
+
+
+# sprintf("strace -ttt -T -e trace=openat,unlink,unlinkat,chdir -o %s -p %s",
+#         paste0(getwd(), ".strace"),
+#         get_pid()) |>
+#   print() |>
+#   system(wait = FALSE)
+#
+# x <- seq(0, 1.0, 0.1); y <- c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1)
+# cbind(x, sprintf("%a", x), sprintf("%a", y))
