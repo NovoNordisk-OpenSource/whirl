ec244ce1f602ea2f1010e494fd8e506c387992f5
LVGK (Lovemore  Gakava) <lvgk@novonordisk.com>
2024-03-14 14:15:58 +0100

    fix: Add option to stop execution of strace if specified as FALSE

--- inst/examples/run_scripts_examples.R
+++ inst/examples/run_scripts_examples.R
@@ -1,14 +1,16 @@
 
 # Example runs calling the logging package --------------------------------
 
-#Define the file name that will be deleted
-fn <- "/scer/homedirs/lvgk/test_output/prgQmd.html"
-fn <- "/scer/homedirs/lvgk/test_output/box_plot.html"
-#Check its existence
-if (file.exists(fn)) {
-  #Delete file if it exists
-  file.remove(fn)
+# Define function to delete logs
+# Delete existing logs
+rm_files <- function(file){
+  if (file.exists(file)) {
+    #Delete file if it exists
+    file.remove(file)
+  }
 }
+
+rm_files("/scer/homedirs/lvgk/test_output/prgQmd.html")
 
 run_script(
   script = retrieve_fpath("prgQmd.qmd"),
@@ -20,6 +22,7 @@ run_script(
 
 "/scer/homedirs/lvgk/test_output/prgQmd.html" |> browseURL()
 
+rm_files("/scer/homedirs/lvgk/test_output/box_plot.html")
 run_script(
   script = "/scer/homedirs/lvgk/training/nn1234/nn1234-0002/current/stats/program/statprog/box_plot.R",
   strace = TRUE,
@@ -30,22 +33,25 @@ run_script(
 
 "/scer/homedirs/lvgk/test_output/box_plot.html" |> browseURL()
 
+
+rm_files("/scer/homedirs/lvgk/test_output/prg1.html")
 run_script(
   retrieve_fpath("prg1.R"),
-  strace = FALSE,
+  strace = TRUE,
   renv = FALSE,
   cleanup = TRUE,
-  output_dir = "./inst/output"
+  output_dir = "../test_output"
 )
 
 "/scer/homedirs/lvgk/test_output/prg1.html" |> browseURL()
 
+rm_files("/scer/homedirs/lvgk/test_output/prgRmd.html")
 run_script(
   retrieve_fpath("prgRmd.Rmd"),
   strace = FALSE,
   renv = FALSE,
   cleanup = TRUE,
-  output_dir = "./inst/output"
+  output_dir = "../test_output"
 )
 
 "/scer/homedirs/lvgk/test_output/prgRmd.html" |> browseURL()
