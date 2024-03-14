f9a6964742d01262b54118c0ec7cb7aa4eb74dea
LVGK (Lovemore  Gakava) <lvgk@novonordisk.com>
2024-03-14 16:10:48 +0100

    Docs: Added to NAMESPACE info for readstrace_info - addition of new function readstrace_info - registered methods for strace_info

---
+++ R/readstrace_info.R
@@ -0,0 +1,65 @@
+#' Get session info
+#'
+#' Retrieve session info and add quarto info if not already there
+#'
+#' @export
+
+readstrace_info <- function(path){
+
+  strace_discards <-
+    c("/lib",
+      "/etc",
+      "/lib64",
+      "/usr",
+      "/var",
+      "/opt",
+      "/sys" ,
+      "/proc",
+      "/tmp",
+      "/.$")
+
+  data_strace <-
+    whirl::read_strace(path, strace_discards = strace_discards) |> dplyr::tibble()
+
+  file_actions <- whirl::refine_strace(data_strace)[c("input", "output")]
+
+  class(file_actions) <- c("whirl_strace_info", class(file_actions))
+  for (i in seq_along(file_actions)) {
+    class(file_actions[[i]]) <- c(paste0("whirl_strace_", names(file_actions)[[i]]), class(file_actions[[i]]))
+  }
+
+  return(file_actions)
+}
+
+
+#' @noRd
+
+knit_print.whirl_strace_info <- function(x, ...){
+
+  x |>
+    lapply(knitr::knit_print)
+}
+
+#' @noRd
+
+knit_print.whirl_strace_input <- function(x, ...){
+
+   x |>
+    dplyr::select("file", "duration", "time") |>
+    knitr::kable(caption = "Input") |>
+    knitr::knit_print()
+
+}
+
+#' @noRd
+
+knit_print.whirl_strace_output <- function(x, ...){
+
+  x |>
+    dplyr::select("file", "duration", "time") |>
+    knitr::kable(caption = "Output") |>
+    knitr::knit_print()
+
+}
+
+
