d8058d60629d79f82f0b03651c2973fdab2a4579
Aksel Thomsen <oath@novonordisk.com>
2024-03-18 18:03:28 +0100

    fix: overwrite old log and keep all strace functions in same script

--- R/log.R
+++ R/log.R
@@ -9,14 +9,30 @@
 
 run_script <- function(script, track_files = FALSE, renv = TRUE, out_dir = dirname(script)) {
 
-  stopifnot(file.exists(script))
-  stopifnot(tools::file_ext(script) %in% c("R", "qmd", "Rmd"))
+  # Input validation
+
+  stopifnot(is.character(script) && file.exists(script) && tools::file_ext(script) %in% c("R", "qmd", "Rmd"))
   stopifnot(is.logical(track_files))
   stopifnot(is.logical(renv))
-  stopifnot(dir.exists(out_dir))
+  stopifnot(is.character(out_dir) && dir.exists(out_dir))
+
+  # Derive execute directory for the quarto render process of the document
+  # Abides to standards for R, Rmd, and qmd scripts,
+  # in order for relative paths to work as expected inside the scripts.
+
+  if (tools::file_ext(script) == "R") {
+    quarto_execute_dir <- getwd()
+  } else {
+    quarto_execute_dir <- normalizePath(dirname(script))
+  }
+
+  # Derive output path
+
+  path_output <- file.path(out_dir, gsub(pattern = "\\.[^\\.]*$", replacement = ".html", x = basename(script)))
 
   # Create temp files for all documents.
   # Note: Documents are copied from package folder to make sure nothing is evaluated there.
+  # Follows recommendation from https://github.com/quarto-dev/quarto-r/issues/81#issuecomment-1375691267
 
   dummy_qmd <- withr::local_tempfile(
     lines = readLines(system.file("documents/dummy.qmd", package = "whirl")),
@@ -52,7 +68,7 @@ run_script <- function(script, track_files = FALSE, renv = TRUE, out_dir = dirna
       output_format = "markdown",
       output_file = basename(doc_md),
       execute_params = list(script = normalizePath(script)),
-      execute_dir = getwd()
+      execute_dir = quarto_execute_dir
     )
   )
 
@@ -71,14 +87,13 @@ run_script <- function(script, track_files = FALSE, renv = TRUE, out_dir = dirna
 
   p$close()
 
-  output <- file.path(out_dir, gsub(pattern = "\\.[^\\.]*$", replacement = ".html", x = basename(script)))
-
   file.copy(
     from = log_html,
-    to = output
+    to = path_output,
+    overwrite = TRUE
   )
 
-  return(invisible(output))
+  return(invisible(path_output))
 }
 
 #' Internal log documents
@@ -95,122 +110,4 @@ log_document <- function(doc) {
 
 log_example <- function(doc) {
   system.file("examples", doc, package = "whirl")
-}
-
-#' Read STRACE file
-#'
-#' @param strace_discards characters to identify records to discard
-#' @param path Path to the .strace file
-#'
-#' @return A `tibble` with strace information.
-#' @export
-#'
-#' @importFrom tibble `%>%`
-#' @importFrom tidyr separate
-
-read_strace <- function(path, strace_discards = c("^/lib", "^/etc", "^/lib64", "^/usr", "^/var", "^/opt", "^/sys", "^/proc", "^/tmp", "^.$", paste0("^", .libPaths()))) {
-  all_strace <- readLines(path)
-
-  strace_filter <- grep("openat\\(AT_FDCWD|unlink\\(|chdir\\(", all_strace, value = TRUE)
-  strace_filter <- grep("ENOENT \\(No such file or directory\\)|ENXIO \\(No such device or address\\)| ENOTDIR \\(Not a directory\\)", strace_filter, value = TRUE, invert = TRUE)
-  # data_strace <- tidyr::separate(data.frame(x = strace_filter), .data$x, sep = '(\\sopenat\\(AT_FDCWD,\\s\\")|(unlink\\(\\")|(chdir\\(\\")',  into = c("time", "file"), fill = "right", remove = FALSE) %>%
-  data_strace <- tidyr::separate(data.frame(x = strace_filter), .data$x, sep = '[^,]\\s|\\([a-zA-Z_,\\s]*\\"', into = c("time", "type", "file"), fill = "right", extra = "merge", remove = TRUE) %>%
-    tidyr::separate(.data$file, sep = "\\)\\s*= ", into = c("file", "num"), remove = FALSE) %>%
-    tidyr::separate(.data$file, sep = '\\", ', into = c("file", "what"), remove = FALSE, fill = "right") %>%
-    tidyr::separate(.data$what, sep = ", ", into = c("what", "access"), remove = FALSE, fill = "right") %>%
-    tidyr::separate(.data$num, sep = " <", into = c("num", "duration"), remove = FALSE, fill = "right")
-
-  data_strace$entrynum <- seq_len(nrow(data_strace))
-  data_strace$file <- gsub('\\"', "", data_strace$file)
-
-  relative_files <- data_strace[!grepl("^/", data_strace$file) & data_strace$type != "chdir", "entrynum"]
-  chdirs <- c(0, data_strace[grepl("^/", data_strace$file) & data_strace$type == "chdir", "entrynum"])
-  rel_chdirs <- c(0, data_strace[!grepl("^/", data_strace$file) & data_strace$type == "chdir", "entrynum"])
-
-  max.ch <- 0
-  rel_dirs <- list()
-  file_paths <- vector()
-  for (i in seq_along(relative_files)) {
-    pos <- chdirs[chdirs < relative_files[i]]
-    max.ch[i] <- pos[length(pos)]
-    rel_dirs[[i]] <- rel_chdirs[max.ch[i] < rel_chdirs & rel_chdirs < relative_files[i]]
-    chdirs <- chdirs[chdirs >= max.ch[i]]
-
-    if (max.ch[i] == 0) {
-      file_paths[i] <- paste(c(getwd(), data_strace$file[c(rel_dirs[[i]], relative_files[i])]), collapse = "/")
-    } else {
-      file_paths[i] <- paste(data_strace$file[c(max.ch[i], rel_dirs[[i]], relative_files[i])], collapse = "/")
-    }
-  }
-
-  data_strace$file[relative_files] <- file_paths
-
-
-  data_strace <- data_strace[grep(paste(strace_discards, collapse = "|"), data_strace$file, invert = TRUE), ]
-
-  if (nrow(data_strace)) {
-    data_strace$time <- as.POSIXct(as.numeric(data_strace$time), origin = "1970-01-01")
-    data_strace$duration <- as.numeric(gsub(">", "", data_strace$duration))
-    data_strace$action <- NA
-    data_strace$action[data_strace$type == "chdir"] <- "Change dir"
-    data_strace$action[is.na(data_strace$what) & data_strace$num == 0 & data_strace$type == "unlink"] <- "Deleted"
-    data_strace$action[is.na(data_strace$action) & grepl("O_DIRECTORY", data_strace$what)] <- "Lookup"
-    data_strace$action[is.na(data_strace$action) & is.na(data_strace$access)] <- "Read"
-    data_strace$action[is.na(data_strace$action) & !is.na(data_strace$access)] <- "Write"
-  }
-
-  data_strace$file <- gsub(normalizePath("~"), "~", data_strace$file)
-
-  return(data_strace[data_strace$type != "chdir", ])
-}
-
-#' refine strace output
-#'
-#' @param data_strace - file lines
-#'
-#' @return tibble
-#' @export
-refine_strace <- function(data_strace) {
-  # remove consecutive duplicates
-
-  rm_dup <- rle(paste(data_strace$file, data_strace$num, data_strace$action))
-  data_strace_s1 <- data_strace[cumsum(c(1, rm_dup$lengths[-length(rm_dup$lengths)])), ]
-
-  # First entry
-  data_strace_s1_first <- data_strace_s1[!duplicated(data_strace_s1$file), ]
-
-  # last entry
-  data_strace_s1_last <- data_strace_s1[!duplicated(data_strace_s1$file, fromLast = TRUE), ]
-
-  # Input files: if first entry is read
-  input_files <- data_strace_s1_first$file[data_strace_s1_first$action == "Read"]
-
-  # Temporary: if write followed by deleted
-  writes <- data_strace_s1$file[data_strace_s1$action == "Write"]
-  last_deleted <- data_strace_s1_last$file[data_strace_s1_last$action == "Deleted"]
-
-  temporary_files <- setdiff(intersect(writes, last_deleted), input_files)
-
-  # Output:
-  output_files <- setdiff(writes, temporary_files)
-
-  # Deleted
-  deleted_files_all <- data_strace_s1$file[data_strace_s1$action == "Deleted"]
-  deleted_files <- setdiff(deleted_files_all, c(input_files, temporary_files, output_files))
-
-  input <- data_strace[data_strace$file %in% input_files, ]
-  output <-  data_strace[data_strace$file %in% output_files, ]
-  temporary <- data_strace[data_strace$file %in% temporary_files, ]
-  deleted <- data_strace[data_strace$file %in% deleted_files, ]
-
-  input <- input[!duplicated(input$file), ]
-  output <- output[!duplicated(output$file), ]
-  temporary <- temporary[!duplicated(temporary$file), ]
-  deleted <- deleted[!duplicated(deleted$file), ]
-
-  list(input = input,
-       output =  output,
-       temporary =  temporary,
-       deleted =  deleted)
-
 }
---
+++ R/strace.R
@@ -0,0 +1,185 @@
+#' Get session info
+#'
+#' Retrieve session info and add quarto info if not already there
+#'
+#' @param path a character vector with path name
+#' @import dplyr
+#'
+#' @export
+
+readstrace_info <- function(path){
+
+  strace_discards <-
+    c("/lib",
+      "/etc",
+      "/lib64",
+      "/usr",
+      "/var",
+      "/opt",
+      "/sys" ,
+      "/proc",
+      "/tmp",
+      "/.$")
+
+  data_strace <-
+    whirl::read_strace(path, strace_discards = strace_discards) |> dplyr::tibble()
+
+  file_actions <- whirl::refine_strace(data_strace)[c("input", "output")]
+
+  class(file_actions) <- c("whirl_strace_info", class(file_actions))
+  for (i in seq_along(file_actions)) {
+    class(file_actions[[i]]) <- c(paste0("whirl_strace_", names(file_actions)[[i]]), class(file_actions[[i]]))
+  }
+
+  return(file_actions)
+}
+
+
+#' @noRd
+
+knit_print.whirl_strace_info <- function(x, ...){
+
+  x |>
+    lapply(knitr::knit_print)
+}
+
+#' @noRd
+
+knit_print.whirl_strace_input <- function(x, ...){
+
+   x |>
+    dplyr::select("file", "duration", "time") |>
+    knitr::kable(caption = "Input") |>
+    knitr::knit_print()
+
+}
+
+#' @noRd
+
+knit_print.whirl_strace_output <- function(x, ...){
+
+  x |>
+    dplyr::select("file", "duration", "time") |>
+    knitr::kable(caption = "Output") |>
+    knitr::knit_print()
+
+}
+
+#' Read STRACE file
+#'
+#' @param strace_discards characters to identify records to discard
+#' @param path Path to the .strace file
+#'
+#' @return A `tibble` with strace information.
+#' @export
+#'
+#' @importFrom tibble `%>%`
+#' @importFrom tidyr separate
+
+read_strace <- function(path, strace_discards = c("^/lib", "^/etc", "^/lib64", "^/usr", "^/var", "^/opt", "^/sys", "^/proc", "^/tmp", "^.$", paste0("^", .libPaths()))) {
+  all_strace <- readLines(path)
+
+  strace_filter <- grep("openat\\(AT_FDCWD|unlink\\(|chdir\\(", all_strace, value = TRUE)
+  strace_filter <- grep("ENOENT \\(No such file or directory\\)|ENXIO \\(No such device or address\\)| ENOTDIR \\(Not a directory\\)", strace_filter, value = TRUE, invert = TRUE)
+  # data_strace <- tidyr::separate(data.frame(x = strace_filter), .data$x, sep = '(\\sopenat\\(AT_FDCWD,\\s\\")|(unlink\\(\\")|(chdir\\(\\")',  into = c("time", "file"), fill = "right", remove = FALSE) %>%
+  data_strace <- tidyr::separate(data.frame(x = strace_filter), .data$x, sep = '[^,]\\s|\\([a-zA-Z_,\\s]*\\"', into = c("time", "type", "file"), fill = "right", extra = "merge", remove = TRUE) %>%
+    tidyr::separate(.data$file, sep = "\\)\\s*= ", into = c("file", "num"), remove = FALSE) %>%
+    tidyr::separate(.data$file, sep = '\\", ', into = c("file", "what"), remove = FALSE, fill = "right") %>%
+    tidyr::separate(.data$what, sep = ", ", into = c("what", "access"), remove = FALSE, fill = "right") %>%
+    tidyr::separate(.data$num, sep = " <", into = c("num", "duration"), remove = FALSE, fill = "right")
+
+  data_strace$entrynum <- seq_len(nrow(data_strace))
+  data_strace$file <- gsub('\\"', "", data_strace$file)
+
+  relative_files <- data_strace[!grepl("^/", data_strace$file) & data_strace$type != "chdir", "entrynum"]
+  chdirs <- c(0, data_strace[grepl("^/", data_strace$file) & data_strace$type == "chdir", "entrynum"])
+  rel_chdirs <- c(0, data_strace[!grepl("^/", data_strace$file) & data_strace$type == "chdir", "entrynum"])
+
+  max.ch <- 0
+  rel_dirs <- list()
+  file_paths <- vector()
+  for (i in seq_along(relative_files)) {
+    pos <- chdirs[chdirs < relative_files[i]]
+    max.ch[i] <- pos[length(pos)]
+    rel_dirs[[i]] <- rel_chdirs[max.ch[i] < rel_chdirs & rel_chdirs < relative_files[i]]
+    chdirs <- chdirs[chdirs >= max.ch[i]]
+
+    if (max.ch[i] == 0) {
+      file_paths[i] <- paste(c(getwd(), data_strace$file[c(rel_dirs[[i]], relative_files[i])]), collapse = "/")
+    } else {
+      file_paths[i] <- paste(data_strace$file[c(max.ch[i], rel_dirs[[i]], relative_files[i])], collapse = "/")
+    }
+  }
+
+  data_strace$file[relative_files] <- file_paths
+
+
+  data_strace <- data_strace[grep(paste(strace_discards, collapse = "|"), data_strace$file, invert = TRUE), ]
+
+  if (nrow(data_strace)) {
+    data_strace$time <- as.POSIXct(as.numeric(data_strace$time), origin = "1970-01-01")
+    data_strace$duration <- as.numeric(gsub(">", "", data_strace$duration))
+    data_strace$action <- NA
+    data_strace$action[data_strace$type == "chdir"] <- "Change dir"
+    data_strace$action[is.na(data_strace$what) & data_strace$num == 0 & data_strace$type == "unlink"] <- "Deleted"
+    data_strace$action[is.na(data_strace$action) & grepl("O_DIRECTORY", data_strace$what)] <- "Lookup"
+    data_strace$action[is.na(data_strace$action) & is.na(data_strace$access)] <- "Read"
+    data_strace$action[is.na(data_strace$action) & !is.na(data_strace$access)] <- "Write"
+  }
+
+  data_strace$file <- gsub(normalizePath("~"), "~", data_strace$file)
+
+  return(data_strace[data_strace$type != "chdir", ])
+}
+
+#' refine strace output
+#'
+#' @param data_strace - file lines
+#'
+#' @return tibble
+#' @export
+
+refine_strace <- function(data_strace) {
+  # remove consecutive duplicates
+
+  rm_dup <- rle(paste(data_strace$file, data_strace$num, data_strace$action))
+  data_strace_s1 <- data_strace[cumsum(c(1, rm_dup$lengths[-length(rm_dup$lengths)])), ]
+
+  # First entry
+  data_strace_s1_first <- data_strace_s1[!duplicated(data_strace_s1$file), ]
+
+  # last entry
+  data_strace_s1_last <- data_strace_s1[!duplicated(data_strace_s1$file, fromLast = TRUE), ]
+
+  # Input files: if first entry is read
+  input_files <- data_strace_s1_first$file[data_strace_s1_first$action == "Read"]
+
+  # Temporary: if write followed by deleted
+  writes <- data_strace_s1$file[data_strace_s1$action == "Write"]
+  last_deleted <- data_strace_s1_last$file[data_strace_s1_last$action == "Deleted"]
+
+  temporary_files <- setdiff(intersect(writes, last_deleted), input_files)
+
+  # Output:
+  output_files <- setdiff(writes, temporary_files)
+
+  # Deleted
+  deleted_files_all <- data_strace_s1$file[data_strace_s1$action == "Deleted"]
+  deleted_files <- setdiff(deleted_files_all, c(input_files, temporary_files, output_files))
+
+  input <- data_strace[data_strace$file %in% input_files, ]
+  output <-  data_strace[data_strace$file %in% output_files, ]
+  temporary <- data_strace[data_strace$file %in% temporary_files, ]
+  deleted <- data_strace[data_strace$file %in% deleted_files, ]
+
+  input <- input[!duplicated(input$file), ]
+  output <- output[!duplicated(output$file), ]
+  temporary <- temporary[!duplicated(temporary$file), ]
+  deleted <- deleted[!duplicated(deleted$file), ]
+
+  list(input = input,
+       output =  output,
+       temporary =  temporary,
+       deleted =  deleted)
+
+}
