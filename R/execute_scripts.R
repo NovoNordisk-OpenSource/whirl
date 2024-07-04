0a6587b5b6efc686b49879e61f28714ca4dbc5ae
LVGK (Lovemore  Gakava) <lvgk@novonordisk.com>
2024-07-04 08:31:43 +0200

    Tests: Added test for execute_scripts

--- R/execute_scripts.R
+++ R/execute_scripts.R
@@ -4,23 +4,50 @@
 #' and Quarto documents (.qmd). It can process individual files or all supported files
 #' in a specified folder, with options for parallel execution.
 #'
-#' @noRd
+#' @param scripts A character vector of file paths to R, R Markdown, or Quarto scripts.
+#'   If NULL, the function will look for scripts in the specified folder.
+#' @param folder A character string specifying the path to a folder containing scripts
+#'   to execute. If provided, all .R, .Rmd, and .qmd files in this folder will be processed.
+#' @param parallel Logical; if TRUE, scripts will be executed in parallel. Default is FALSE.
+#' @param num_cores Integer specifying the number of cores to use for parallel execution.
+#'   If NULL (default), it will use one less than the total number of available cores.
+#' @param ... Other arguments passed on to the run_script function,
+#'
+#' @return A list containing the execution results for each script. Each element of the
+#'   list is a character string indicating the success or failure of the script execution.
+#'
+#' @importFrom rmarkdown render
+#' @importFrom quarto quarto_render
+#' @importFrom parallel detectCores makeCluster stopCluster parLapply clusterEvalQ
+#' @export
 execute_scripts <- function(scripts = NULL,
                             folder = NULL,
                             parallel = FALSE,
                             num_cores = NULL,
                             summary_dir = getwd(),
                             ...) {
+  if (!is.null(num_cores) && (!is.numeric(num_cores) || num_cores <= 0)) {
+    stop("Invalid input for 'num_cores'. Please provide a positive numeric value.")
+  }
+
+  if (!is.null(summary_dir) && !dir.exists(summary_dir)) {
+    stop("The specified summary directory does not exist.")
+  }
+
   if (!is.null(folder)) {
-    # If a folder is specified, get all supported script files in the folder
-    script_files <- list.files(path = folder,
-                               pattern = "\\.(R|Rmd|qmd)$",
-                               full.names = TRUE)
-  } else if (is.character(scripts)) {
-    # If scripts is a character vector, assume it's a list of file paths
+    if (!dir.exists(folder)) {
+      stop("The specified folder path does not exist.")
+    }
+    # If a folder is specified and it exists, get all supported script files in the folder
+    script_files <- list.files(path = folder, pattern = "\\.(R|Rmd|qmd)$", full.names = TRUE)
+  } else if (!is.null(scripts)) {
+    if (!is.character(scripts)) {
+      stop("Invalid input for 'scripts'. Please provide a character vector of script paths.")
+    }
+    # If 'scripts' is a character vector, assume it's a list of file paths
     script_files <- scripts
   } else {
-    stop("Invalid input. Please provide either a vector of script paths or a folder path.")
+    stop("Missing input. Please provide either a vector of script paths or a folder path.")
   }
 
   # Function to execute a single script
@@ -28,19 +55,21 @@ execute_scripts <- function(scripts = NULL,
     cat("Executing script:", script, "\n")
 
     result <- tryCatch({
-      # source(script, local = TRUE)
       output <- run_script(script, ...)
-      dplyr::tibble(
+      tibble::tibble(
         Directory = dirname(normalizePath(script, winslash = "/")),
         Filename = basename(normalizePath(script, winslash = "/")),
         Status = output$status$status,
         Hyperlink = normalizePath(output$log_details$location, winslash = "/")
       )
     }, error = function(e) {
-      paste("Error executing script:",
-            script,
-            "\nError message:",
-            conditionMessage(e))
+      tibble::tibble(
+        Directory = dirname(normalizePath(script, winslash = "/")),
+        Filename = basename(normalizePath(script, winslash = "/")),
+        Status = "error",
+        Hyperlink = NA_character_,
+        ErrorMessage = conditionMessage(e)
+      )
     })
 
     return(result)
@@ -54,16 +83,10 @@ execute_scripts <- function(scripts = NULL,
 
     cat("Executing scripts in parallel using", num_cores, "cores\n")
 
-    cl <- makeCluster(num_cores)
+    cl <- parallel::makeCluster(num_cores)
 
-    # Define a function to execute a single script with progress display
-    execute_single_script_with_progress <- function(script) {
-      cat("Executing script:", script, "\n")  # Display progress
-      execute_single_script(script)  # Execute the script
-    }
-
-    results <- parLapply(cl, script_files, execute_single_script_with_progress)
-    stopCluster(cl)
+    results <- parallel::parLapply(cl, script_files, execute_single_script)
+    parallel::stopCluster(cl)
 
   } else {
     # Sequential execution
@@ -80,7 +103,7 @@ execute_scripts <- function(scripts = NULL,
   summary_qmd <- withr::local_tempfile(
     lines = readLines(system.file("documents/summary.qmd", package = "whirl")),
     fileext = ".qmd"
-    )
+  )
 
   summary_log_html <- withr::local_tempfile(fileext = ".html")
 
@@ -96,10 +119,10 @@ execute_scripts <- function(scripts = NULL,
       input = summary_qmd,
       output_format = "html_document",
       output_file = summary_log_html,
-      params = list(summary_df = summary_df, summary_dir = summary_dir_f)
+      params = list(summary_df = summary_df, summary_dir = summary_dir_f),
+      quiet = TRUE
     )
   )
-
 
   # Create requested outputs
 
@@ -114,6 +137,7 @@ execute_scripts <- function(scripts = NULL,
   return(invisible(summary_df))
 }
 
+
 #' @noRd
 knit_print.whirl_summary_info <- function(x, path_rel_start, ...) {
   hold <- x |>
@@ -124,7 +148,7 @@ knit_print.whirl_summary_info <- function(x, path_rel_start, ...) {
 
   hold <- hold |>
     dplyr::mutate(formated = ifelse(
-      grepl("posit.cloud", Sys.getenv("RS_SERVER_URL")),
+      grepl("rstudio_cloud", Sys.getenv("R_CONFIG_ACTIVE")),
       file.path("/file_show?path=", .data[["Hyperlink"]]),
       file.path(fs::path_rel(.data[["Hyperlink"]], start = path_rel_start))
     ))
