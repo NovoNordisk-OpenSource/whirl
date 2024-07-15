4ae7b34442893426bb02d30ee13637848ba2a30c
CGID (Cervan Girard) <CGID@novonordisk.com>
2024-07-15 11:45:41 +0200

    wip

---
+++ inst/examples/sequence_exuction/error/prg1.R
@@ -0,0 +1,21 @@
+
+#' Setup
+
+library(tidyverse)
+
+#' Data data
+
+x <- mtcars |>
+  as_tibble(rownames = "car")
+
+x |>
+  print(n = 100)
+
+message("this is good")
+
+#' Plot
+
+ggplot(data = x) +
+  geom_point(mapping = aes(x = mpg, y = hp, size = wt, colour = as.factor(am)))
+
+ggsave("plot1.png")
