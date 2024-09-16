c9a46f1b2b2619230d2f284764e25ed8e6aba991
LVGK (Lovemore Gakava) <lvgk@novonordisk.com>
2024-09-16 12:51:58 +0200

    fix: lintr-cyclocomp_linter warning of function util_queue_summary should have a cyclomatic complexity of less than 15

--- R/util_queue_summary.R
+++ R/util_queue_summary.R
@@ -1,56 +1,42 @@
 #' Generate a summary tibble from a queue table
 #'
-#' This function takes a queue table as input and generates a summary tibble with columns for Directory, Filename, Status, Hyperlink, and Information.
+#' This function takes a queue table as input and generates a summary tibble
+#' with columns for Directory, Filename, Status, Hyperlink, and Information.
 #'
 #' @param queue_table The queue table containing the result data
 #' @return A tibble summarizing the queue table data
 #' @export
 util_queue_summary <- function(queue_table) {
-  if (!"result" %in% names(queue_table) || !is.list(queue_table$result)) {
+  if (!"result" %in% names(queue_table) ||
+        !is.list(queue_table$result)) {
     stop("queue_table must contain a list named 'result'")
   }
 
-  if (!all(sapply(queue_table$result, function(x) all(c("log_details", "status") %in% names(x))))) {
+  if (!all(sapply(queue_table$result, function(x) {
+    all(
+      c("log_details", "status") %in% names(x)
+    )
+  }))) {
     stop("Each result in queue_table must contain 'log_details' and 'status'")
   }
 
   tibble::tibble(
     Directory = sapply(queue_table[["result"]], function(x) {
-      if ("log_details" %in% names(x) && "script" %in% names(x$log_details)) {
-        normalizePath(dirname(x$log_details$script), winslash = "/")
-      } else {
-        NA
-      }
+      normalizePath(dirname(x$log_details$script), winslash = "/")
     }),
     Filename = sapply(queue_table[["result"]], function(x) {
-      if ("log_details" %in% names(x) && "script" %in% names(x$log_details)) {
-        basename(x$log_details$script)
-      } else {
-        NA
-      }
+      basename(x$log_details$script)
     }),
     Status = sapply(queue_table[["result"]], function(x) {
-      if ("status" %in% names(x) && "status" %in% names(x$status)) {
-        x$status$status
-      } else {
-        NA
-      }
+      x$status$status
     }),
     Hyperlink = sapply(queue_table[["result"]], function(x) {
-      if ("log_details" %in% names(x) && "location" %in% names(x$log_details)) {
-        x$log_details$location
-      } else {
-        NA
-      }
+      x$log_details$location
     }),
     Information = sapply(queue_table[["result"]], function(x) {
-      if ("status" %in% names(x) && "warning" %in% names(x$status)) {
-        x$status$warning
-      } else {
-        NULL
-      }
+      x$status$warning
     })
   ) |>
-    tidyr::unnest(cols = c(Information), keep_empty = TRUE) |>
+    tidyr::unnest(cols = c("Information"), keep_empty = TRUE) |>
     tidyr::replace_na(list(Information = ""))
 }
