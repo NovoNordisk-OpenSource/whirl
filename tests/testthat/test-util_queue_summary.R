d3e50650fee016562390f3d7280d09c239cca6c0
LVGK (Lovemore Gakava) <lvgk@novonordisk.com>
2024-09-16 11:59:02 +0200

    feat: - utility function to create summary log tibble - Add script information in the r_session output - add test for utility function

---
+++ tests/testthat/test-util_queue_summary.R
@@ -0,0 +1,28 @@
+# Test for existence of 'result' in the queue_table
+test_that("queue_table must contain a list named 'result'", {
+  queue_table <- list(dummy = "dummy")
+  expect_error(util_queue_summary(queue_table), "queue_table must contain a list named 'result'")
+})
+
+# Test for existence of 'log_details' and 'status' in each result
+test_that("Each result in queue_table must contain 'log_details' and 'status'", {
+  queue_table <- list(result = list(list(log_details = "dummy")))
+  expect_error(util_queue_summary(queue_table), "Each result in queue_table must contain 'log_details' and 'status'")
+})
+
+# Test for successful creation of summary tibble
+test_that("Summary tibble is created successfully", {
+  q <- whirl_queue$new(n_workers = 3)
+
+  c(
+    "inst/examples/simple/prg1.R",
+    "inst/examples/simple/success.R"
+  ) |>
+    q$run()
+
+  queue_table <- q$queue
+  summary_tibble <- util_queue_summary(queue_table)
+  expect_type(summary_tibble, "list")
+  expect_equal(ncol(summary_tibble), 5)
+  expect_equal(nrow(summary_tibble), 2)
+})
