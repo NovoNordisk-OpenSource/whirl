a3b66c854dfbde9a979ed98fea26a2481cae5517
oath_nngithub <oath@novonordisk.com>
2024-09-20 15:21:09 +0200

    fix: review comments

--- inst/examples/simple/log_msg.R
+++ inst/examples/simple/log_msg.R
@@ -2,6 +2,7 @@
 library(whirl)
 
 #' This is a test script for user log messages
+#' Read, write, and delete operations can be logged the following way:
 
 log_read("adam.adsl")
 log_read("sdtm.vs")
@@ -14,22 +15,10 @@ log_write("adam.advs")
 
 log_write("output.table.ext")
 
-#' Is this interface nice for the user or would it better with:
-
-# whirl::log_read("adam.adsl")
-# whirl::log_write("adam.advs")
-
-#' And no support for custom messages?
+#' Behind the scenes the log messages are stored in a temporary file to be
+#' read into the log report.
 #'
-#' Make them available as standalone functions to plug into a package
-#' without having to import whirl.
-#'
-#' See:
-#'
-#' - [usethis::use_standalone()](https://usethis.r-lib.org/reference/use_standalone.html)
-#' - [standalone-rlang](https://github.com/r-lib/rlang/blob/main/R/standalone-rlang.R)
-#'
-#' Show how it looks behind the scenes:
+#' The file can be accessed via the environment variable "WHIRL_LOG_MSG":
 
 Sys.getenv("WHIRL_LOG_MSG")
 
