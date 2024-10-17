0ed90da8358f82af2b32f5b0a0d31a4d1aa773d1
akselthomsen <thomsenaksel@gmail.com>
2024-10-17 14:13:25 +0200

    feat: add use_whirl utility function

---
+++ tests/testthat/test-use_whirl.R
@@ -0,0 +1,23 @@
+test_that("use_whirl", {
+  withr::with_tempdir({
+    rlang::local_interactive(FALSE)
+
+    usethis::create_project(path = ".")
+
+    use_whirl()
+
+    expect_true(file.exists("_whirl.yaml"))
+
+    expect_equal(
+      readLines("_whirl.yaml"),
+      readLines(system.file("use_whirl/_whirl.yaml", package = "whirl"))
+    )
+
+    expect_true(file.exists(".gitignore"))
+
+    expect_contains(
+      readLines(".gitignore"),
+      "*_log.(html|json|md)"
+    )
+  })
+})
