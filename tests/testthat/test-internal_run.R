dd016dcf096ffaef7e74393dea950d14c3a39b65
oath_nngithub <oath@novonordisk.com>
2024-11-11 17:24:08 +0100

    test: remove unnecessary tempdir usage

--- tests/testthat/test-internal_run.R
+++ tests/testthat/test-internal_run.R
@@ -2,18 +2,16 @@ test_that("testing internal_run()", {
   file_config <- system.file("examples/demo/tfl/tfl_whirl.yaml", package = "whirl")
   config_to_config <- system.file("examples/demo/config_to_config.yaml", package = "whirl")
 
-  #A config file
-  withr::with_tempdir({
-    queue <- whirl_queue$new()
-    internal_run(input = file_config, steps = NULL, level = 1, queue = queue) |>
-      expect_no_error()
-  })
+  # A config file
 
-  #A config file
-  withr::with_tempdir({
-    queue <- whirl_queue$new()
-    internal_run(input = config_to_config, steps = NULL, level = 1, queue = queue) |>
-      expect_no_error()
-  })
+  queue <- whirl_queue$new()
+  internal_run(input = file_config, steps = NULL, level = 1, queue = queue) |>
+    expect_no_error()
+
+  # A config file calling another config file
+
+  queue <- whirl_queue$new()
+  internal_run(input = config_to_config, steps = NULL, level = 1, queue = queue) |>
+    expect_no_error()
 
 })
