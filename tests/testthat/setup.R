cb9c31d082033e494141c8b30eeb3433a6d35391
oath_nngithub <oath@novonordisk.com>
2024-11-12 15:14:21 +0100

    test: upgrade never

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,6 +1,6 @@
 ## whirl have to be installed for tests
 
-devtools::install()
+devtools::install(upgrade = "never")
 
 # pkgs <- installed.packages() |> row.names()
 # test <- any(pkgs %in% "whirl")
