93c6b6f154d006e76ec0056f3528e702612d91db
oath_nngithub <oath@novonordisk.com>
2024-11-12 14:52:57 +0100

    test: force install before testing

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,13 @@
 ## whirl have to be installed for tests
-pkgs <- installed.packages() |> row.names()
-test <- any(pkgs %in% "whirl")
 
-if (!test) {
-  cli::cli_abort("The package whirl have to be installed for subprocesses")
-}
+devtools::install()
+#
+# pkgs <- installed.packages() |> row.names()
+# test <- any(pkgs %in% "whirl")
+#
+# if (!test) {
+#   cli::cli_abort("The package whirl have to be installed for subprocesses")
+# }
 
 # Minimal prints to make it easier to read test output
 
