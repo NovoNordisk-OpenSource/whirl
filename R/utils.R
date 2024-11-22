06f4bf2901be870aa3cb544d22962266dc42379e
akselthomsen <thomsenaksel@gmail.com>
2024-11-22 15:59:34 +0100

    fix: R CMD Check

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,7 @@ get_file_ext <- function(file_paths) {
     FUN = function(file_path) {
       file_name <- basename(file_path)
       file_parts <- strsplit(file_name, "\\.")[[1]]
-      file_extension <- ifelse(length(file_parts) == 1, "", tail(file_parts, 1))
+      file_extension <- ifelse(length(file_parts) == 1, "", utils::tail(file_parts, 1))
       return(file_extension)
     },
     FUN.VALUE = character(1),
