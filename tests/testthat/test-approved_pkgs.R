6c55cf959e1550e0223f8a7c3a13dc77e88d4e7c
akselthomsen <thomsenaksel@gmail.com>
2025-04-04 12:40:58 +0200

    fix: make sure test is getting a single input

--- tests/testthat/test-approved_pkgs.R
+++ tests/testthat/test-approved_pkgs.R
@@ -1,11 +1,11 @@
 test_that("Running approved_pkgs workflow", {
-  url <- getOption("repos")
+  url <- getOption("repos")[[1]]
   status <- check_url(sprintf("%s/src/contrib/PACKAGES", url))
 
   skip_if(!status)
 
   result <- check_approved(
-    approved_pkg_folder = .libPaths(),
+    approved_pkg_folder = .libPaths()[[1]],
     approved_pkg_url = url,
     session_pkgs = sessioninfo::session_info()$packages
   ) |>
