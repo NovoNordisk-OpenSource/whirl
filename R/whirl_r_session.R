b8d358a2d58c73fc577410dbd7995c5f8cecf596
akselthomsen <thomsenaksel@gmail.com>
2025-06-25 12:44:49 +0200

    test: if it handles ci error on mac

--- R/whirl_r_session.R
+++ R/whirl_r_session.R
@@ -205,6 +205,7 @@ wrs_finalize <- function(self, private, super) {
   zephyr::msg_debug(
     "Finalizing session with pid={.field {self$get_pid()}} and wd={.file {private$wd}}" # nolint: line_length_linter
   )
+  super$read() # Just make sure it is ready to run below code
   super$run(func = setwd, args = list(dir = getwd()))
   unlink(private$wd, recursive = TRUE)
   super$finalize()
