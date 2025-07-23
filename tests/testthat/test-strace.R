814d73379a994113f43083ce18c64b031233ef77
ovlr <OVLR@novonordisk.com>
2025-07-23 15:43:59 +0200

    trying to fail the program again

--- tests/testthat/test-strace.R
+++ tests/testthat/test-strace.R
@@ -9,53 +9,19 @@ strace_info <- function(path = "strace.log") {
 
 test_that("strace works", {
   skip_on_cran()
-  #skip_on_ci()
+  skip_on_ci()
   skip_on_os(c("windows", "mac", "solaris"))
-
-  if (Sys.getenv("CI") != "") {
-    cat("Running in CI environment\n")
-    cat("ptrace_scope:", readLines("/proc/sys/kernel/yama/ptrace_scope"), "\n")
-    cat("Kernel version:", system("uname -r", intern = TRUE), "\n")
-
-    # Use safer methods to get user info
-    tryCatch({
-      if (exists("Sys.getuid", mode = "function")) {
-        cat("User ID:", Sys.getuid(), "\n")
-      } else {
-        cat("Sys.getuid() not available\n")
-      }
-    }, error = function(e) cat("Error getting UID:", e$message, "\n"))
-
-    tryCatch({
-      if (exists("Sys.geteuid", mode = "function")) {
-        cat("Effective user ID:", Sys.geteuid(), "\n")
-      } else {
-        cat("Sys.geteuid() not available\n")
-      }
-    }, error = function(e) cat("Error getting EUID:", e$message, "\n"))
-
-    # Alternative ways to get user info
-    cat("USER env var:", Sys.getenv("USER"), "\n")
-    cat("LOGNAME env var:", Sys.getenv("LOGNAME"), "\n")
-    cat("whoami output:", system("whoami", intern = TRUE), "\n")
-    cat("id output:", system("id", intern = TRUE), "\n")
-
-    # Test strace directly
-    cat("Testing strace on simple command:\n")
-    strace_test <-  system("timeout 2s strace -e trace=write echo 'test' 2>&1", intern = TRUE)
-    cat("Strace test result:\n")
-    cat(paste(strace_test, collapse = "\n"), "\n")
-  }
 
   withr::with_tempdir(
     code = {
       cat("this is a dummy file to check strace", file = "dummy.txt")
 
-      p <-  callr::r_session$new()
+      p <- callr::r_session$new()
 
       start_strace(pid = p$get_pid(), file = file.path(getwd(), "strace.log"))
 
       # Only save a file
+
       p$run(\() saveRDS(object = mtcars, file = "mtcars.rds"))
 
       test <- strace_info()
@@ -64,17 +30,19 @@ test_that("strace works", {
         testthat::expect_true()
 
       # Also read dummy.txt
+
       p$run(\() readLines("dummy.txt"))
-      test <-  strace_info()
+      test <- strace_info()
       any(grepl(x = test$write$file, pattern = "mtcars.rds")) |>
         testthat::expect_true()
       any(grepl(x = test$read$file, pattern = "dummy.txt")) |>
         testthat::expect_true()
 
       # Finally delete read dummy.txt
+
       p$run(\() file.remove("dummy.txt"))
 
-      test <-  strace_info()
+      test <- strace_info()
       any(grepl(x = test$delete$file, pattern = "dummy.txt")) |>
         testthat::expect_true()
 
