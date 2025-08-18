b525b6897a1e8792c7c78a546c1b0f479a6f1f7a
akselthomsen <thomsenaksel@gmail.com>
2025-08-18 15:05:57 +0200

    fix: cleanup

--- inst/documents/python_modules.py
+++ inst/documents/python_modules.py
@@ -1,33 +1,59 @@
-# mypy: disable-error-code="name-defined,assignment,call-overload"
-# pylint: disable=undefined-variable
 import json
-import subprocess  # nosec B404
 import sys
+import types
 
-temp_dir = r.params["tmpdir"]  # noqa: F821 # pyright: ignore [reportUndefinedVariable]
+def get_loaded_packages():
+    """Return all the packages that are currently loaded in the Python environment"""
+    loaded = set(m.split('.')[0] for m in sys.modules if m and not m.startswith('_'))
+    return loaded
 
-# Get a list of installed packages using pip list
-installed_packages = (
-    subprocess.check_output([sys.executable, "-m", "pip", "list", "-v"])  # nosec B603
-    .decode("utf-8")
-    .strip()
-    .split("\n")[2:]
-)
-installed_packages = {
-    package.split()[0]: [package.split()[1], package.split()[2]]
-    for package in installed_packages
-}
+def get_namespaced_packages():
+    """Get all the stuff that exposed via namespacing (i.e. attached, like import pandas)"""
+    namespaced = set()
+    
+    # Use __main__.__dict__ to find all imported modules in the current script needed for things like reticulate
+    import __main__
+    if hasattr(__main__, '__dict__'):
+        globals_dict = __main__.__dict__
+    else:
+        frame = sys._getframe(1)
+        globals_dict = frame.f_globals
+    
+    for name, obj in globals_dict.items():
+        if isinstance(obj, types.ModuleType) and not name.startswith('_'):
+            module_name = getattr(obj, '__name__', name)
+            root_module = module_name.split('.')[0]
+            if root_module not in ['sys', 'json', 'types', 'builtins']:
+                namespaced.add(root_module)
+    
+    return namespaced
 
-imported_modules = {}
-for module_name, module in sys.modules.items():
-    if not (
-        module_name.startswith("_") or module_name.startswith(".") or "." in module_name
-    ):
-        if module_name in installed_packages:
-            imported_modules[module_name] = {
-                "version": installed_packages[module_name][0],
-                "installation_path": installed_packages[module_name][1],
-            }
+def get_package_status(tmpfile=''):
+    """Get both loaded and namespaced packages as a named list"""
+    loaded = get_loaded_packages()
+    namespaced = get_namespaced_packages()
+    
+    status = {
+        'loaded': list(loaded),
+        'namespaced': list(namespaced)
+    }
+    
+    if tmpfile != '':
+        with open(tmpfile, 'w') as f:
+            json.dump(status, f, indent=2)
+    
+    return status
 
-with open(f"{temp_dir}/python_imports.json", "w") as json_file:
-    json.dump(imported_modules, json_file)
+# TODO: What is this? A unit test?
+if __name__ == "__main__":
+    pkg1_status = get_package_status('old_status.json')
+    
+    import pandas as pds
+    
+    pkg2_status = get_package_status('new_status.json')
+    
+    new_loaded = set(pkg2_status['loaded']) - set(pkg1_status['loaded'])
+    new_namespaced = set(pkg2_status['namespaced']) - set(pkg1_status['namespaced'])
+    
+    print("New loaded packages:", new_loaded)
+    print("New namespaced items:", new_namespaced)
