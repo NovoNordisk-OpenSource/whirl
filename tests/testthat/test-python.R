ac531601c81e680b41adb9d612348eec3095578c
akselthomsen <thomsenaksel@gmail.com>
2025-08-19 15:33:24 +0200

    test

--- tests/testthat/test-python.R
+++ tests/testthat/test-python.R
@@ -3,7 +3,6 @@ test_that("python dependencies found correctly", {
   skip_if_no_quarto()
   skip_if_no_python()
 
-  reticulate::use_virtualenv()
   reticulate::py_require("pandas")
   reticulate::py_require("numpy")
 
