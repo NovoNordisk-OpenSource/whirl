f151473eb760d01b0deaf849eb6047e48c5a93aa
akselthomsen <thomsenaksel@gmail.com>
2025-08-21 11:02:42 +0200

    feat: add unit tests for no python imports

--- tests/testthat/test-python.R
+++ tests/testthat/test-python.R
@@ -6,19 +6,34 @@ test_that("python dependencies found correctly", {
   reticulate::py_require("pandas")
   reticulate::py_require("numpy")
 
-  res <- test_script("py_dependencies.py") |>
+  res <- test_script(
+    script = c("py_success.py", "py_dependencies.py")
+  ) |>
     run(summary_file = NULL) |>
     expect_no_warning() |>
     expect_no_error()
 
-  res[["status"]][[1]] |>
-    expect_equal("success")
+  res[["status"]] |>
+    expect_equal(c("success", "success"))
 
-  info <- res[["result"]][[1]][["session"]]
+  info_py_success <- res[["result"]][[1]][["session"]]
 
-  info$platform$setting |>
+  info_py_success$platform$setting |>
     expect_contains("python")
 
-  info$python$package[info$python$namespaced] |>
+  info_py_success$python$package[
+    info_py_success$python$directly_used
+  ] |>
+    expect_length(0)
+
+  info_py_dependencies <- res[["result"]][[2]][["session"]]
+
+  info_py_dependencies$platform$setting |>
+    expect_contains("python")
+
+  info_py_dependencies$python$package[
+    info_py_dependencies$python$directly_used
+  ] |>
+    expect_length(2) |>
     expect_contains(c("pandas", "numpy"))
 })
