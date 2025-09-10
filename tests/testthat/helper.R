28076b20217b5a7473c0361afd8338afbd18f9dc
ovlr <OVLR@novonordisk.com>
2025-09-10 13:28:22 +0200

    might work

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,13 +1,8 @@
 # Helper function to select test scripts
 
-test_script <- function(script) {
-  if (Sys.info()[["sysname"]] == "Windows") {
-    script <- testthat::test_path("scripts", script) |>
-      normalizePath(winslash = "\\", mustWork = TRUE)
-  } else {
-    script <-  testthat::test_path("scripts", script) |>
-      normalizePath(winslash = "/", mustWork = TRUE)
-  }
+test_script <-  function(script) {
+  script <- testthat::test_path("scripts", script) |>
+    normalizePath(winslash = "/", mustWork = TRUE)
   return(script)
 }
 
