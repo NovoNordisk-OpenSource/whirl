4e83ed405289691de5d886498b9a5bb5f2fd8ac1
ovlr <OVLR@novonordisk.com>
2025-09-11 10:10:47 +0200

    Final commit

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,6 @@
 # Helper function to select test scripts
 
-test_script <-  function(script) {
+test_script <- function(script) {
   script <- testthat::test_path("scripts", script) |>
     normalizePath(winslash = "/", mustWork = TRUE)
   return(script)
