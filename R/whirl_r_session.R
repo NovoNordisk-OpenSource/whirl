0b2f1107444efadd51a8c7ee1f2ac2b13d4db2fc
cgid <cgid@novonordisk.com>
2025-10-07 14:19:30 +0200

    from a clean session

--- R/whirl_r_session.R
+++ R/whirl_r_session.R
@@ -125,6 +125,9 @@ wrs_initialize <- function(
   private,
   super
 ) {
+  options_called <- callr::r_session_options()
+  options_called$libpath <- .libPaths()
+  super$initialize(wait_timeout = wait_timeout, options = options_called)
   super$initialize(wait_timeout = wait_timeout) # uses callr::r_session$initialize()
 
   private$.tmpdir <- withr::local_tempdir(clean = FALSE)
